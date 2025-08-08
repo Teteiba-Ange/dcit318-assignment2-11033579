66e9e61fd30d2fbb0afd13ea6f1327e0edc55398
Teteiba-Ange <aacquah041@st.ug.edu.gh>
2025-08-08 10:58:40 +0100

    On the child class

---
+++ dcit318-assignment2-11033579.cs/Program.cs
@@ -0,0 +1,19 @@
+﻿// code for inheritance
+using System;
+class Animal { 
+
+    public void  makeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+ class Dog :Animal
+{
+     public  void makeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+
